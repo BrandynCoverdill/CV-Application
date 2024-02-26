e1ea3d60f2242765da3f5014b06080e94ed6af68
Brandyn Coverdill <brandyncoverdill98@gmail.com>
2024-02-26 00:07:25 -0700

    reinstalled uuid and normalize.css

--- cv-application/src/components/Experience.jsx
+++ cv-application/src/components/Experience.jsx
@@ -1,5 +1,10 @@
 import '../styles/Experience.css';
 
-export default function Experience() {
-	return;
+export default function Experience({ work }) {
+	return (
+		<section className='experience'>
+			<h2>Work Experience</h2>
+			<ul></ul>
+		</section>
+	);
 }
