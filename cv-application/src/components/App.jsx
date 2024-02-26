98471ea5576a2f4de092b799a7c77cea8dfc3608
Brandyn Coverdill <brandyncoverdill98@gmail.com>
2024-02-26 00:43:46 -0700

    Added Experience component

--- cv-application/src/components/App.jsx
+++ cv-application/src/components/App.jsx
@@ -1,6 +1,6 @@
 import '../styles/App.css';
-import {v4 as uuidv4} from 'uuid';
-import {useState} from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { useState } from 'react';
 import Header from './Header';
 import Summary from './Summary';
 import Qualifications from './Qualifications';
--- cv-application/src/components/Experience.jsx
+++ cv-application/src/components/Experience.jsx
@@ -4,7 +4,24 @@ export default function Experience({ work }) {
 	return (
 		<section className='experience'>
 			<h2>Work Experience</h2>
-			<ul></ul>
+			{work.map((w) => {
+				return (
+					<div key={w.id}>
+						<p>
+							{w.startDate} - {w.endDate}
+						</p>
+						<p>{w.companyTitle}</p>
+						<p>
+							{w.companyName} - {w.companyCity}, {w.companyState}
+						</p>
+						<ul>
+							{w.duties.map((duty) => {
+								return <li>{duty}</li>;
+							})}
+						</ul>
+					</div>
+				);
+			})}
 		</section>
 	);
 }
