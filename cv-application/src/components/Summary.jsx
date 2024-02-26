b64bd39e150edcb7abb627050efab62dfe164261
Brandyn Coverdill <brandyncoverdill98@gmail.com>
2024-02-26 04:36:25 -0700

    Added summary elements on isEditing

--- cv-application/src/components/Summary.jsx
+++ cv-application/src/components/Summary.jsx
@@ -1,10 +1,26 @@
 import '../styles/Summary.css';
 
 export default function Summary({ person, isEditing }) {
-	return (
-		<section className='summary'>
-			<h2>Summary Statement</h2>
-			<p>{person.statement}</p>
-		</section>
-	);
+	if (isEditing) {
+		return (
+			<section className='summary'>
+				<h2>Summary Statement</h2>
+				<p>
+					<textarea
+						name='statement'
+						id='statement'
+						cols='50'
+						rows='5'
+					></textarea>
+				</p>
+			</section>
+		);
+	} else {
+		return (
+			<section className='summary'>
+				<h2>Summary Statement</h2>
+				<p>{person.statement}</p>
+			</section>
+		);
+	}
 }
