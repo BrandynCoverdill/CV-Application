a0d8e2736be9e82e87c6c57ec5d885f760375bf2
Brandyn Coverdill <brandyncoverdill98@gmail.com>
2024-02-26 05:10:06 -0700

    Added qualification elements on isEditing

--- cv-application/src/components/Qualifications.jsx
+++ cv-application/src/components/Qualifications.jsx
@@ -1,14 +1,34 @@
 import '../styles/Qualifications.css';
+import Button from './Button';
 
 export default function Qualifications({ person, isEditing }) {
-	return (
-		<section className='qualifications'>
-			<h2>Core Qualifications</h2>
-			<ul>
-				{person.qualifications.map((qualification) => {
-					return <li>{qualification}</li>;
-				})}
-			</ul>
-		</section>
-	);
+	if (isEditing) {
+		return (
+			<section className='qualificationsEdit'>
+				<h2>Core Qualifications</h2>
+				<ul>
+					{person.qualifications.map((qualification) => {
+						return (
+							<>
+								<input type='text' placeholder='Qualification' />
+								<button className='deleteBtn'>Delete</button>
+							</>
+						);
+					})}
+					<button className='addBtn'>Add new Qualification</button>
+				</ul>
+			</section>
+		);
+	} else {
+		return (
+			<section className='qualifications'>
+				<h2>Core Qualifications</h2>
+				<ul>
+					{person.qualifications.map((qualification) => {
+						return <li>{qualification}</li>;
+					})}
+				</ul>
+			</section>
+		);
+	}
 }
