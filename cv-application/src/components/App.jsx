b616d6e6e4f594f13c29bdbec4d1f14bc51fadd9
Brandyn Coverdill <brandyncoverdill98@gmail.com>
2024-02-25 18:42:29 -0700

    Added education and modified package.json file

--- cv-application/src/components/App.jsx
+++ cv-application/src/components/App.jsx
@@ -1,10 +1,11 @@
 import '../styles/App.css';
-import { useState } from 'react';
+import {v4 as uuidv4} from 'uuid';
+import {useState} from 'react';
 import Header from './Header';
 import Summary from './Summary';
 import Qualifications from './Qualifications';
-// import Education from './Education';
-// import Experience from './Experience';
+import Education from './Education';
+import Experience from './Experience';
 
 export default function App() {
 	const [person, setPerson] = useState({
@@ -15,7 +16,7 @@ export default function App() {
 		state: 'WA',
 		zipcode: '12345',
 		statement: `
-Hey there, I'm Alex, and I'm absolutely fascinated by web development. The blend of technology and creativity just lights me up. I've always had a knack for design, so the idea of crafting visually stunning websites while also diving deep into coding to make them function flawlessly is incredibly exciting to me. I'm particularly drawn to the challenge of mastering both the front-end and back-end aspects of development. I want to create not just visually appealing sites but also ensure they're intuitive and user-friendly, with seamless functionality. Ultimately, I see myself in a career where I can keep learning and innovating, pushing the boundaries of what's possible in web development.`,
+Hey there, I'm Everett, and I'm absolutely fascinated by web development. The blend of technology and creativity just lights me up. I've always had a knack for design, so the idea of crafting visually stunning websites while also diving deep into coding to make them function flawlessly is incredibly exciting to me. I'm particularly drawn to the challenge of mastering both the front-end and back-end aspects of development. I want to create not just visually appealing sites but also ensure they're intuitive and user-friendly, with seamless functionality. Ultimately, I see myself in a career where I can keep learning and innovating, pushing the boundaries of what's possible in web development.`,
 		qualifications: [
 			'HTML',
 			'CSS',
@@ -36,6 +37,7 @@ Hey there, I'm Alex, and I'm absolutely fascinated by web development. The blend
 
 	const [school, setSchool] = useState([
 		{
+			key: uuidv4(),
 			qualification: 'Software Development',
 			schoolName: 'Stanford University',
 			schoolCity: 'Stanford',
@@ -45,6 +47,7 @@ Hey there, I'm Alex, and I'm absolutely fascinated by web development. The blend
 
 	const [work, setWork] = useState([
 		{
+			key: uuidv4(),
 			startDate: '2018',
 			endDate: 'Current',
 			companyTitle: 'Software Engineer',
@@ -67,8 +70,8 @@ Hey there, I'm Alex, and I'm absolutely fascinated by web development. The blend
 			<Header person={person} />
 			<Summary person={person} />
 			<Qualifications person={person} />
-			{/* <Education /> */}
-			{/* <Experience /> */}
+			<Education school={school} />
+			<Experience work={work} />
 		</>
 	);
 }
--- cv-application/src/components/Education.jsx
+++ cv-application/src/components/Education.jsx
@@ -1,5 +1,20 @@
+import {Fragment} from 'react';
 import '../styles/Education.css';
 
-export default function Education() {
-	return;
+export default function Education({school}) {
+	return (
+		<section className='education'>
+			<h2>Education</h2>
+			{school.map((s) => {
+				return (
+					<Fragment key={s.key}>
+						<p>
+							<b>{s.qualification}</b> - {s.schoolName}
+						</p>
+						<p>{s.schoolCity + ', ' + s.schoolState}</p>
+					</Fragment>
+				);
+			})}
+		</section>
+	);
 }
